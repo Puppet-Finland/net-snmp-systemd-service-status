05580e76752ebd68953222ae728f7d8af72c1ece
Samuli Seppänen <samuli.seppanen@puppeteers.net>
2021-08-06 08:11:15 +0000

    Convert script to pass_persist to boost performance by 5x
    Signed-off-by: Samuli Seppänen <samuli.seppanen@puppeteers.net>

--- README.md
+++ README.md
@@ -1,41 +1,25 @@
 # net-snmp-systemd-service-status
 
-This is a snmpd (Net-SNMP) "pass" extension script that generates "is service
-up" data for all detected systemd services. On a high level it works as follows:
+This is a snmpd (Net-SNMP) "pass_persist" extension script that generates "is
+service up" data for all detected systemd services. On a high level it works as
+follows:
 
-1. Use a private OID subtree as a prefix for all OIDs
-1. List systemd service unit files on the system
-1. Filter out services that can have multiple instances ("@.service")
-1. Get status ("is-active") for each unit
+1. Uses a private OID subtree as a prefix for all OIDs
+1. Lists systemd service unit files on the system
+1. Filters out services that can have multiple instances ("@.service")
+1. Gets the numeric return value of systemctl is-active for each service
 1. Convert the name of the service into a valid OID by changing every character into its equivalent decimal ASCII code
-1. Sort the OIDs (to prevent snmpwalk from choking on unordered OIDs)
-1. Write the sorted OID data (oid, "integer", return value) to a cache file
-1. Write the current index to a index file
+1. Store all data (oid, data type, value, service name) in a dictionary
+1. Create a sorted list of oids used for snmpwalk / getnext
+1. Provide snmpd the data it requests by listening on standard input and answering queries as documented in snmpd.conf
 
-Cache and index files are necessary because snmpd launches the script once per
-get and getnext request and the script's state is lost in between incantations.
-
-When a "get" request is sent print the data in the cache file that
-matches the requested OID.
-
-When a "getnext" request is sent print the data on the current line, then
-increment the index by one.
-
-The cache file is not updated on every incantation. Instead, it has a lifetime
-after which it will be refreshed. This has little practical effect except when
-testing the script manually, as you want the latest status anyways in real-life
-scenarios.
-
-This script can be used interactively as well. Run it without any parameters to see command-line help.
+This script can be used interactively as well. No parameters are needed, just
+talk to the script like described in snmpd.conf.
 
 # Performance
 
-The performance of this script is as good as it can be when doing a simple
-snmpget. When doing an snmpwalk it is a lot slower because snmpd launches the
-script on every getnext request. In practice walking through ~180 systemd
-services takes <10 seconds on a low-end virtual machine.
-
-To boost performance this script would have to rewritten to work as
-pass-persist script which snmpd would only launch once. Alternatively there
-would have to be a filter to target only the systemd services you're really
-interested in.
+This script was originally a "pass" script, which meant that snmpd would launch
+the script once per "getnext" request. On a relatively low-powered Ubuntu 18.04
+VM with 180 systemd service  getting systemd service status took about 10
+seconds. With the current pass_persist approach querying only takes about 1.6
+seconds.
--- systemd-service-status.py
+++ systemd-service-status.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-import linecache
+import logging
 import os
 import re
 import subprocess
@@ -8,97 +8,56 @@ import time
 
 class SystemdServiceStatus:
 
-    def __init__(self, oid_prefix, cache_file, cache_lifetime, index_file):
-        self.cache_file = cache_file
-        self.cache_lifetime = cache_lifetime
-        self.index_file = index_file
-        self.cache = None
+    def __init__(self, oid_prefix):
+        """Object containing information about systemd services and their status"""
         self.oid_prefix = oid_prefix
 
-        # Get the latest index. This is required for getnext().
-        self.index = 1
-        try:
-            with open(index_file, "r") as f:
-                self.index = int(f.readline().rstrip())
-        except FileNotFoundError:
-            pass
+        # self.data is a dictionary that contains all systemd service data using programmatically
+        # generated OID as the key:
+        #
+        # --- snip ---
+        # '1.3.9950.1.115.115.115.100.45.115.101.99.114.101.116.115': ('integer', 3, 'sssd-secrets'),
+        # '1.3.9950.1.115.115.115.100': ('integer', 0, 'sssd'),
+        # --- snip ---
+        self.data = {}
 
-        # Cache service status to a file
+        # self.sorted_oids enables iterating over the OIDs in correct order
+        # while having the data in a non-ordered dictionary (self.data).
+        self.sorted_oids = []
+
+        # Get systemd service status and store it in self.data. Also populate
+        # self.sorted_oids.
         self.cache_service_status()
 
     def cache_service_status(self):
-        """Create or refresh service status cache if needed"""
+        """Cache service status"""
 
-        if os.path.exists(self.cache_file):
-            if float(time.time()) - os.path.getmtime(self.cache_file) > self.cache_lifetime and self.index == 1:
-                os.remove(self.cache_file)
-            else:
-                return
+        # Temporary data structure used to create sorted oid list later
+        oids = [self.oid_prefix]
 
         lines = subprocess.check_output(["/bin/systemctl", "list-unit-files", "-t", "service", "--no-legend"]).decode("UTF-8").split("\n")
- 
-        oids = []
-        data = {}
 
         for line in lines:
+            # Filter out systemd services that are service instance "templates" and
+            # not real services, e.g. openvpn@.service.
             if line and not "@." in line:
-               service_name = re.sub(r'\.service\s+\w+$', '', line) 
+               service_name = re.sub(r'\.service\s+\w+\s*$', '', line) 
                service_oid = self.create_oid(service_name)
                try:
                    service_status = subprocess.check_call(["/bin/systemctl", "is-active", service_name], stdin=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
                except subprocess.CalledProcessError as cpe:
                    service_status = cpe.returncode
 
-               data[service_oid] = ('integer', service_status, service_name)
+               self.data[service_oid] = ('integer', service_status, service_name)
                oids.append(service_oid)
 
         # Sort the oids array. Lovingly borrowed from here:
         #
         # https://rosettacode.org/wiki/Sort_a_list_of_object_identifiers#Python
         #
-        # The array sorted array is then used to write the data to the file
-        # in correct order.
-        oids = sorted(oids, key=lambda x: list(map(int, x.split('.'))))
-        self.write_cache(data, oids)
-
-    def write_cache(self, data, oids):
-        """Write sorted cache to disk"""
-        cache = open(self.cache_file, "a+")
-        for oid in oids:
-            output = ".%s %s %s %s\n" % (oid, data[oid][0], data[oid][1], data[oid][2])
-            cache.write(output)
-
-    def get(self, oid):
-        """Print OID, data type and service status return code for OID"""
-        self.cache = open(self.cache_file, "r")
-
-        for line in self.cache.readlines():
-            data = line.split(" ")
-            if data[0] == oid:
-                self.print_data(data)
-
-    def getnext(self):
-        """Get the next OID from the cache"""
-        data = linecache.getline(self.cache_file, self.index).split(" ")
-        if data == ['']:
-            self.index = 1
-            self.update_index()
-            sys.exit(0)
-        else:
-            self.print_data(data)
-            self.index += 1
-            self.update_index()
-
-    def update_index(self):
-        """Update the index file"""
-        with open(self.index_file, "w+") as f:
-            f.write(str(self.index))
-
-    def print_data(self, data):
-        """Print data in the format snmpd wants it"""
-        print(data[0])
-        print(data[1])
-        print(data[2])
+        # The array sorted array is then used to output data in the correct order when using
+        # getnext (snmpwalk)
+        self.sorted_oids = sorted(oids, key=lambda x: list(map(int, x.split('.'))))
 
     def create_oid(self, name):
         """Generate oid programmatically from the service name"""
@@ -110,27 +69,63 @@ class SystemdServiceStatus:
 
         return oid.lstrip(".")
 
+def getline():
+    return sys.stdin.readline().strip()
+
+def output(line):
+    sys.stdout.write(line + "\n")
+    sys.stdout.flush()
+
 def main():
     oid_prefix = "1.3.9950.1"
-    cache_file = "/var/lib/snmp/systemd-service-status.cache"
-    index_file = "/var/lib/snmp/systemd-service-status.index"
-    cache_lifetime = 240
 
+    s = SystemdServiceStatus(oid_prefix)
+
+    # The main loop that talks with snmpd. Mostly taken from
+    #
+    # https://net-snmp.sourceforge.io/wiki/index.php/Tut:Extending_snmpd_using_shell_scripts#Pass_persist
     try:
-        op = sys.argv[1]
-        oid = sys.argv[2]
-    except IndexError:
-        print("Usage: systemd-service-status.py [-g|-n] [OID]")
-        sys.exit(1)
+        while True:
+            command = getline()
 
-    s = SystemdServiceStatus(oid_prefix, cache_file, cache_lifetime, index_file)
+            if command == "":
+                sys.exit(0)
 
-    if op == "-g":
-        s.get(oid)
-    elif op == "-n":
-        s.getnext()
-    else:
-        print("ERROR: unknown operation %s" % (op))
+            # snmpd 5.4.2.1 sends a PING before every snmp command
+            elif command == "PING":
+                output("PONG")
+
+            elif command == "set":
+                oid = getline()
+                type_and_value = getline()
+                output("not-writable")
+
+            elif command == "get":
+                oid = getline()
+                output(str(oid))
+                output(str(s.data[oid.lstrip(".")][0]))
+                output(str(s.data[oid.lstrip(".")][1]))
+
+            elif command == "getnext":
+                oid = getline()
+                ni = s.sorted_oids.index(oid.lstrip(".")) + 1
+                output("." + str(s.sorted_oids[ni]))
+                output(str(s.data[s.sorted_oids[ni]][0]))
+                output(str(s.data[s.sorted_oids[ni]][1]))
+
+            else:
+                pass
+                logging.error("Unknown command: %s" % command)
+
+    # If we get an exception, spit it out to the log then quit
+    # (by propagating exception).
+    # snmpd will restart the script on the next request.
+    except Exception as ep:
+        f = open("/var/lib/snmp/systemd-service-status.err.log", "a")
+        f.write(ep)
+        f.close()
+        logging.error(e)
+        raise
 
 if __name__ == "__main__":
     main()
